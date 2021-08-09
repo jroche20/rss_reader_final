f793c5a8a6c97d74b745ac2bd5f344f902a078fa
Jackie <jroche@berkeley.edu>
2021-08-09 10:53:17 -0700

    first commit

---
+++ rss-reader/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <form @submit.prevent="getRss">
+      <div>
+        <label> rss url</label>
+        <br />
+        <input v-model="rssUrl" />
+      </div>
+      <input type="submit" />
+    </form>
+    <div v-for="item of items" :key="item.title">
+      <h1>{{ item.title }}</h1>
+      <p>{{ item.author }}</p>
+      <a :href="item.link">{{ item.link }}</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      rssUrl: "",
+      items: [],
+    };
+  },
+  methods: {
+    async getRss() {
+      const urlRegex = /(http|ftp|https)://[\w-]+(.[\w-]+)+([\w.,@?^=%&amp;:/~+#-]*[\w@?^=%&amp;/~+#-])?/;
+      if (!urlRegex.test(this.rssUrl)) {
+        return;
+      }
+      const res = await fetch(
+        `https://api.allorigins.win/get?url=${this.rssUrl}`
+      );
+      const { contents } = await res.json();
+      const feed = new window.DOMParser().parseFromString(contents, "text/xml");
+      const items = feed.querySelectorAll("item");
+      this.items = [...items].map((el) => ({
+        link: el.querySelector("link").innerHTML,
+        title: el.querySelector("title").innerHTML,
+        author: el.querySelector("author").innerHTML,
+      }));
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
